b4bcb274e376622b81edb51c00404ffa2c452658
yiroote <laiyibin@laiyibindeMacBook-Pro.local>
2016-06-18 18:52:08 +0800

    项目终结
    last

---
+++ 衣洛特/FirstTableView.swift
@@ -0,0 +1,157 @@
+import UIKit
+
+class FirstTableView:UITableView,UITableViewDelegate, UITableViewDataSource,
+First_ScrollView_Protocol{
+    //用于保存所有消息
+    var bubbleSection:Array<FirstTableMessageItem>!
+    //数据源，用于与 ViewController 交换数据
+    var imageArray :[String!] = ["衣洛特iOS图标/beautiful1.png","衣洛特iOS图标/beautiful2.png","衣洛特iOS图标/beautiful3.png","衣洛特iOS图标/beautiful4.png"]
+    var First_Scroll_Title :[String!] = ["beautiful1.png","iOS图标/beautiful2.png","衣洛特iOS图标/beautiful3.png","衣洛特iOS图标/beautiful4.png"]
+    weak var didSelectDelegate : First_Table_Delegate?
+    //拉刷新控制器
+    var refreshControl = UIRefreshControl()
+    var timer:NSTimer!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    init(frame:CGRect)
+    {
+        self.bubbleSection = Array<FirstTableMessageItem>()
+        
+        super.init(frame:frame,  style:UITableViewStyle.Plain)
+
+        var First_Scroll = First_ScrollView()
+        First_Scroll = First_ScrollView(frame: CGRectMake(0, 0, self.frame.width, 200*高比例),defaultTimeInterval: 3)
+        First_Scroll.backgroundColor = UIColor.clearColor()
+        First_Scroll.delegate = self
+        self.backgroundColor = UIColor.grayColor()
+        self.tableHeaderView = First_Scroll
+        self.separatorStyle = UITableViewCellSeparatorStyle.None//分隔符风格
+        self.delegate = self
+        self.dataSource = self
+        
+        //添加刷新
+        refreshControl.addTarget(self, action: "refreshData",
+            forControlEvents: UIControlEvents.ValueChanged)
+        refreshControl.attributedTitle = NSAttributedString(string: "下拉刷新数据")
+        self.addSubview(refreshControl)
+        
+        
+        MySQL.shareMySQL().removeAllDynamic()
+        refreshData()
+    }
+ 
+    // 刷新数据
+    func refreshData() {
+        //移除老数据
+
+        //导入本地数据
+        searchDynamicToFirstTable()
+        //刷新远程数据
+        updateNewDynamic()
+        
+    }
+    
+    func searchDynamicToFirstTable(){
+        //self.bubbleSection
+        self.bubbleSection=MySQL.shareMySQL().searchDynamic()
+    }
+    
+    func updateNewDynamic(){
+        var ind:Int=1
+        let str:String = MySQL.shareMySQL().lastDateDynamic()
+        downDynamic.sharedDownDynamic()?.conNet(str,block:{(dataInfo,data) -> Void in
+            if dataInfo == "已经是最新的"
+            {
+                print("updateDynamicFirst() 已经是最新的")
+            }
+            else if dataInfo == "有更新数据"
+            {
+                print("有更新数据，开始往table和本地数据库导入")
+                for i in 0..<data.count
+                {
+                    var tempdata = data[i]
+                    let temp = FirstTableMessageItem(dynamic_id:data[i][0],FirstTableImage: data[i][2],FirstTableTitle: data[i][7],FirstTableDetail: data[i][3],FirstTable_yanjin_Num: Int(data[i][4])!,FirstTable_pinglun_Num: Int(data[i][5])!)
+                    self.bubbleSection.append(temp)
+                    MySQL.shareMySQL().insertDynamic(data[i])
+                    downDynamic.sharedDownDynamic()?.downDynamicImage(data[i][2],dynamic_id: data[i][0])
+                }
+            }
+            else if dataInfo == "网络连接错误"
+            {
+                print("FirstTable网络错误")
+                MBProgressHUD.showDelayHUDToView(self, message: "网络连接错误")
+            }
+            //获取数据完毕
+            super.reloadData()
+            self.reloadData()
+            self.refreshControl.endRefreshing()
+        })
+        
+    }
+    
+    
+    override func reloadData()
+    {
+        
+        self.showsVerticalScrollIndicator = true
+        self.showsHorizontalScrollIndicator = true//滚动的提示条
+        
+        super.reloadData()
+    }
+    //第一个方法返回分区数，在本例中，就是1
+    func numberOfSectionsInTableView(tableView:UITableView)->Int
+    {
+        return 1
+    }
+    
+    /** First_ScrollViewDelegate*/
+    func First_numberOfPages() -> Int {
+        
+        return imageArray.count;
+    }
+    func First_currentPageViewIndex(index: Int) -> String {
+        
+        return imageArray[index]
+    }
+    func First_didSelectCurrentPage(index: Int) {
+        
+        
+    }
+    func First_currentPageLabel_Title(index: Int) -> String {
+        return First_Scroll_Title[index]
+    }
+
+    
+    //返回指定分区的行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.bubbleSection.count
+    }
+        
+    //用于确定单元格的高度，如果此方法实现得不对，单元格与单元格之间会错位
+    func tableView(tableView:UITableView,heightForRowAtIndexPath  indexPath:NSIndexPath) -> CGFloat
+    {
+        
+        return 140
+    }
+
+    
+    //返回自定义的 TableViewCell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+          
+            let cellId = "MsgCell"
+            let data =  self.bubbleSection[indexPath.row]
+            let image_frame = CGRectMake(0, 0, self.frame.width, 120)
+            let cell =  FirstTableViewCell(frame:image_frame,data:data, reuseIdentifier:cellId)
+            return cell
+
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("shuashuashuashua")
+        self.didSelectDelegate?.First_Table_DidSelect(indexPath.row,bubbleSection: self.bubbleSection[indexPath.row])
+    }
+}
---
+++ 衣洛特/shezhi.swift
@@ -0,0 +1,110 @@
+//
+//  shezhi.swift
+//  衣洛特
+//
+//  Created by __________V|R__________ on 16/2/19.
+//  Copyright © 2016年 __________V|R__________. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class shezhiView: UIView {
+    
+    var blackBackGround = UIButton()
+    var shezhikuang = UIButton()
+    var cleanOtherUser = UIButton()
+    var cleanCache = UIButton()
+    var chatUs = UIButton()
+    var chatTextView=UITextView()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        blackBackGround.backgroundColor = UIColor.blackColor()
+        blackBackGround.alpha = 0.5
+        blackBackGround.frame = CGRectMake(0, 20, self.frame.width, self.frame.height)
+        blackBackGround.addTarget(self, action: "back", forControlEvents: UIControlEvents.TouchUpInside)
+        self.addSubview(blackBackGround)
+        
+        shezhikuang.frame = CGRectMake(100, self.frame.height, self.frame.width-200, 400)
+        shezhikuang.setBackgroundImage(UIImage(named: "衣洛特iOS图标/设置界面/设置中间背景.jpg"), forState: UIControlState.Normal)
+        shezhikuang.setBackgroundImage(UIImage(named: "衣洛特iOS图标/设置界面/设置中间背景.jpg"), forState: UIControlState.Highlighted)
+        self.addSubview(shezhikuang)
+        
+        cleanOtherUser.frame=CGRectMake(110, -50, self.frame.width-220, 30)
+        cleanOtherUser.setTitle("清除其他用户", forState: UIControlState.Normal)
+        cleanOtherUser.tintColor=UIColor.whiteColor()
+        cleanOtherUser.addTarget(self, action: "CleanOtherUser", forControlEvents: UIControlEvents.TouchUpInside)
+        self.addSubview(cleanOtherUser)
+        
+        cleanCache.frame=CGRectMake(110, 0, self.frame.width-220, 30)
+        cleanCache.setTitle("清除缓存", forState: UIControlState.Normal)
+        cleanCache.tintColor=UIColor.whiteColor()
+        self.addSubview(cleanCache)
+        
+        chatTextView.frame=CGRectMake(110, self.frame.height+200-30-10-100, self.frame.width-220, 100)
+        chatTextView.backgroundColor=UIColor.whiteColor()
+        self.addSubview(chatTextView)
+        
+        chatUs.frame=CGRectMake(110, self.frame.height+200-30, self.frame.width-220, 30)
+        chatUs.setTitle("联系我们", forState: UIControlState.Normal)
+        chatUs.addTarget(self, action: "chat", forControlEvents: UIControlEvents.TouchUpInside)
+        chatUs.tintColor=UIColor.whiteColor()
+        self.addSubview(chatUs)
+
+        
+        UIView.animateWithDuration(1, // 动画时长
+            delay:0 ,// 动画延迟z
+            usingSpringWithDamping:1.0 ,// 类似弹簧振动效果 0~1
+            initialSpringVelocity:1.0 ,// 初始速度
+            options:UIViewAnimationOptions.CurveEaseIn, // 动画过渡效果
+            animations: {()-> Void in
+                self.shezhikuang.frame = CGRectMake(100, 150, self.frame.width-200, self.frame.height-300)
+                self.cleanOtherUser.frame=CGRectMake(110, 180, self.frame.width-220, 30)
+                self.cleanCache.frame=CGRectMake(110, 230, self.frame.width-220, 30)
+                self.chatTextView.frame=CGRectMake(110, 280, self.frame.width-220, 100)
+                self.chatUs.frame=CGRectMake(110, 400, self.frame.width-220, 30)
+            }, completion: nil)
+        
+    }
+    
+    func chat()
+    {
+        
+    }
+    
+    func CleanOtherUser()
+    {
+        let username:String=NSUserDefaults.standardUserDefaults().objectForKey("username") as! String
+        Clean.sharedLoginModel()?.cleanOtherUser(username)
+    }
+    
+    func back()
+    {
+        UIView.animateWithDuration(1, // 动画时长
+            delay:0 ,// 动画延迟z
+            usingSpringWithDamping:1.0 ,// 类似弹簧振动效果 0~1
+            initialSpringVelocity:1.0 ,// 初始速度
+            options:UIViewAnimationOptions.CurveEaseIn, // 动画过渡效果
+            animations: {()-> Void in
+                self.shezhikuang.frame = CGRectMake(self.frame.width/2-50, -100,100, 100)
+                self.cleanOtherUser.frame=CGRectMake(self.frame.width/3+10, self.frame.height, self.frame.width/3-20, 30)
+                self.cleanCache.frame=CGRectMake(self.frame.width/3+10, self.frame.height+50, self.frame.width/3-20, 30)
+                self.chatUs.frame=CGRectMake(self.frame.width/3+10, -30, self.frame.width/3-20, 30)
+            }, completion: {(finnish)-> Void in
+                self.blackBackGround.removeFromSuperview()
+                self.shezhikuang.removeFromSuperview()
+                self.cleanOtherUser.removeFromSuperview()
+                self.cleanCache.removeFromSuperview()
+                self.chatUs.removeFromSuperview()
+                self.removeFromSuperview()
+        })
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+
+
+}
+
